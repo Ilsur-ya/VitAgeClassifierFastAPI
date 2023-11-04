72035f2ed981f86ef72cad84a9f4d304fac065aa
yusupov ilsur <ilsur.ya@mail.ru>
2023-11-04 15:06:05 +0300

    tests

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import time
+import pytest
+from main import app
+from fastapi.testclient import TestClient
+
+
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    yield client
+
+
--- tests/mytests.py
+++ tests/mytests.py
@@ -1,34 +1,29 @@
-from main import app
 
-from fastapi.testclient import TestClient
-
-
-client = TestClient(app)
 def test_old_man(client):
     response = client.get(
         "/get-age-by-photo",
-        query_string={
+        params={
             'url': 'https://indasil.club/uploads/posts/2022-11/1669494194_45-indasil-club-p-portret-starogo-cheloveka-instagram-51.jpg',
         }
     )
     assert response.status_code == 200
-    assert response.json == {
+    assert response.json() == {
         'Age': 'more than 70'}
 
 def test_young_man(client):
     response = client.get(
         "/get-age-by-photo",
-        query_string={
+        params={
             'url': 'https://luckclub.ru/images/luckclub/2019/03/newborn-baby-asleep-1.jpg',
         }
     )
     assert response.status_code == 200
-    assert response.json == {
+    assert response.json() == {
         'Age': '0-2'}
 def test_empty_query(client):
     response = client.get(
         "/get-age-by-photo",
-        query_string={
+        params={
             'url': '',
         }
     )
